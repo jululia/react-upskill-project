475d5af79aa521e6352830268b88e46fd6f1ac0d
Julia Reibring <julia.reibring@klarna.com>
2024-02-27 23:55:23 +0100

    Animated header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -20,19 +20,35 @@ export const Header = () => {
     const calcSize = (startScrolling-scrollPosition) > 50 ? 250-scrollPosition : 50;
     const calcFontSize = (startScrolling-scrollPosition) > 225 ? 50-scrollPosition : 25;
     const flexGrow = (startScrolling-scrollPosition) > 50 ? 1 : 0;
-    const showBottomBorder = (startScrolling- scrollPosition) > 50 ? "transparent" : "grey";
-    const alignText = (startScrolling- scrollPosition) > 50 ? "center" : "left";
-    const backgroudColor = (startScrolling- scrollPosition) > 50 ? "transparent" : "white";
+    const showBottomBorder = (430- scrollPosition) > 70 ? "transparent" : "grey";
+    const alignText = (430- scrollPosition) > 70 ? "center" : "center";
+    const backgroudColor = (430- scrollPosition) > 70 ? "transparent" : "white";
+    const divHeight =  (430- scrollPosition) > 70 ? 430-scrollPosition : 70;
+    const leftSun =  (430- scrollPosition) > 70 ? 50+40/430*scrollPosition : 90;
+    const leftTitle =  (430- scrollPosition) > 70 ? 50-40/430*scrollPosition : 0;
+    const transformTitle =  (430- scrollPosition) > 70 ? -50 : -50;
+    const titleTop =  (430- scrollPosition) > 70 ? 90 : 50;
+    const sunTop =  (430- scrollPosition) > 40 ? 40+scrollPosition/430*10 : 50;
+    
+    
 
-    // const myStyle = {
-    //     color: 'red',
-    //     fontSize: '20px'
-    // };
+    
+   
 
     return (
-        <div className="header" style={{borderColor:showBottomBorder, alignText: alignText, backgroundColor: backgroudColor }}>
-            <img className="header-logo" src="sun.svg" width={calcSize} height={calcSize} style={{flexGrow: flexGrow} } alt="Logo" />
-            <h1 className="header-title" style={{fontSize: `${calcFontSize}px`, alignText: alignText, flexGrow: flexGrow} }>All about the sun</h1>
+        <div className="header" style={{borderColor:showBottomBorder, alignText: alignText, backgroundColor: backgroudColor, height: divHeight}}>
+            <img className="header-logo" src="sun.svg" width={calcSize} height={calcSize} style={{
+                flexGrow: flexGrow, 
+                left: `${leftSun}%`,
+                top: `${sunTop}%`} } alt="Logo" />
+            <h1 className="header-title" style={{
+                // transform: `translate(${transformTitle}%, -50%)`, 
+                fontSize: `${calcFontSize}px`, 
+                // alignText: alignText, 
+                // flexGrow: flexGrow, 
+                left: `${50}%`,
+                top: `${titleTop}%`
+                } }>All about the sun</h1>
         </div>
     );
 };

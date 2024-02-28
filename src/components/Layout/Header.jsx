a93aea5676968c8d168efe20cf6730d3837aaaad
Julia Reibring <julia.reibring@klarna.com>
2024-02-28 10:42:41 +0100

    Tiny tweaks

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -20,11 +20,11 @@ export const Header = () => {
     const calcSize = (startScrolling-scrollPosition) > 50 ? 250-scrollPosition : 50;
     // const calcFontSize = (startScrolling-scrollPosition) > 225 ? 25 : 25;
     const flexGrow = (startScrolling-scrollPosition) > 70 ? 1-scrollPosition/430*1 : 0;
-    const showBottomBorder = (430- scrollPosition) > 70 ? "transparent" : "grey";
-    const alignText = (430- scrollPosition) > 70 ? "center" : "center";
+    const showBottomBorder = (430- scrollPosition) > 0 ? "transparent" : "lightgrey";
+    const alignText = (430- scrollPosition) > 60 ? "center" : "center";
     const backgroudColor = (430- scrollPosition) > 70 ? "transparent" : "white";
     const divHeight =  (430- scrollPosition) > 70 ? 430-scrollPosition : 70;
-    const leftSun =  (430- scrollPosition) > 0 ? 50+scrollPosition/430*40 : 90;
+    const leftSun =  (430- scrollPosition) > 70 ? 50+scrollPosition/360*40 : 90;
     const leftTitle =  (430- scrollPosition) > 70 ? 50-40/430*scrollPosition : 0;
     const transformTitle =  (430- scrollPosition) > 70 ? -50 : -50;
     const titleTop =  (430- scrollPosition) > 70 ? 90-scrollPosition/430*40 : 50;

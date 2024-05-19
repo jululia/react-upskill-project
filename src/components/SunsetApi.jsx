23ae62992b7d12798dd7d4dfab55e21ff6f28396
julia.reibring <julia.reibring@klarna.com>
2024-05-19 14:42:45 +0200

    Thumbnail icon and app information.

--- src/components/SunsetApi.jsx
+++ src/components/SunsetApi.jsx
@@ -138,9 +138,6 @@ export const SunsetApi = ({ city, latitude, longitude, timezone, showDetailsButt
           <h2 style={{ display: "inline" }}>
           {city === "Where you are" ? `Sunset where you are ${isSunsetPast ? "is" : "is not"} at ${formattedSunsetTime} today.` :
             `Sunset in ${city} ${isSunsetPast ? "is" : "is not"} at ${formattedSunsetTime} today.`}
-
-      {/* {city === "Where you are"? Sunset where you are {isSunsetPast} at {formattedSunsetTime} today.:
-            Sunset in {city} {isSunsetPast} at {formattedSunsetTime} today.} */}
           </h2>
           {showDetailsButton === true && (
             <Link to={`/city/${city.toLowerCase()}`} style={{ display: "inline" }} >
